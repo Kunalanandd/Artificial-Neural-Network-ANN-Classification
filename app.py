9bd6298cc520b83fedb30966b3f8f2dd0a3b9297
Kunalanandd <kunalannand9045@gmail.com>
2025-10-26 17:00:52 +1100

    Add files via upload

--- app.py
+++ app.py
@@ -23,6 +23,21 @@ with open('scaler.pkl','rb') as file:
 
 ## Streamapp
 st.title("Customer Churn Predection")
+st.markdown("""
+###  About This Model
+This application uses an **Artificial Neural Network (ANN)** trained on customer data to predict the **likelihood of customer churn** — that is, whether a customer is likely to leave the service.
+
+The model considers various features such as:
+- **Credit Score, Age, Tenure, Balance,** and **Estimated Salary**
+- **Number of Products**, **Credit Card status**, and **Activity level**
+- **Geography** and **Gender**
+
+After processing these inputs, the model produces a **churn probability score (0–1)**.  
+A higher score (above 0.5) indicates that the customer is **likely to churn**, while a lower score suggests they are **likely to stay**.
+
+>  This ANN was trained using TensorFlow/Keras and scaled input data via scikit-learn’s `StandardScaler`, ensuring consistent feature normalization for accurate predictions.
+""")
+
 
 geography = st.selectbox('geography', one_hot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
